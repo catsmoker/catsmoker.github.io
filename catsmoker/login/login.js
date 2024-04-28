b3806316a7bd0d0cd5a797bcb25b480ddb7f7541
catsmoker <119059457+catsmoker@users.noreply.github.com>
2024-04-28 01:41:30 +0100

    Create login.js

---
+++ catsmoker/login/login.js
@@ -0,0 +1,15 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  // Get input values
+  var username = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
+
+  // Check username and password
+  if (username === 'catsmoker' && password === 'catsmoker123@') {
+    // Redirect to another site
+    window.location.href = 'http://catsmoker.ddns.net/'; // Replace 'https://example.com' with the URL you want to redirect to
+  } else {
+    alert('Invalid username or password. Please try again.');
+  }
+});
