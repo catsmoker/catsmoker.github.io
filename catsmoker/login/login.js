63f541314181b197cbb49cf0f3f7fb399a8f5d50
catsmoker <119059457+catsmoker@users.noreply.github.com>
2024-04-28 17:03:42 +0100

    Update login.js

--- catsmoker/login/login.js
+++ catsmoker/login/login.js
@@ -8,7 +8,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
   // Check username and password
   if (username === 'catsmoker' && password === 'catsmoker123@') {
     // Redirect to another site
-    window.location.href = 'ftp://catsmoker.ddns.net/'; // Replace with the URL you want to redirect to
+    window.location.href = 'http://catsmoker.ddns.net/'; // Replace with the URL you want to redirect to
   } else {
     alert('Invalid username or password. Please try again.');
   }
