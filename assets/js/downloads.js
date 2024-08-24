c7ea58c70c05111427d7716721485c8ab1b8f39b
catsmoker <119059457+catsmoker@users.noreply.github.com>
2024-08-24 16:05:08 +0100

    Add files via upload

---
+++ assets/js/downloads.js
@@ -0,0 +1,78 @@
+function filterLinks() {
+    const searchTerm = document.getElementById('searchBox').value.toLowerCase();
+    const sections = document.querySelectorAll('.card-container');
+
+    sections.forEach(section => {
+        const title = section.querySelector('h2').textContent.toLowerCase();
+        if (searchTerm === '' || title.includes(searchTerm)) {
+            section.classList.remove('hidden');
+        } else {
+            section.classList.add('hidden');
+        }
+    });
+}
+
+window.tailwind.config = {
+    darkMode: 'class', // Use 'class' for dark mode
+    theme: {
+        extend: {
+            colors: {
+                border: 'hsl(var(--border))',
+                input: 'hsl(var(--input))',
+                ring: 'hsl(var(--ring))',
+                background: 'hsl(var(--background))',
+                foreground: 'hsl(var(--foreground))',
+                primary: {
+                    DEFAULT: 'hsl(var(--primary))',
+                    foreground: 'hsl(var(--primary-foreground))'
+                },
+                secondary: {
+                    DEFAULT: 'hsl(var(--secondary))',
+                    foreground: 'hsl(var(--secondary-foreground))'
+                },
+                destructive: {
+                    DEFAULT: 'hsl(var(--destructive))',
+                    foreground: 'hsl(var(--destructive-foreground))'
+                },
+                muted: {
+                    DEFAULT: 'hsl(var(--muted))',
+                    foreground: 'hsl(var(--muted-foreground))'
+                },
+                accent: {
+                    DEFAULT: 'hsl(var(--accent))',
+                    foreground: 'hsl(var(--accent-foreground))'
+                },
+                popover: {
+                    DEFAULT: 'hsl(var(--popover))',
+                    foreground: 'hsl(var(--popover-foreground))'
+                },
+                card: {
+                    DEFAULT: 'hsl(var(--card))',
+                    foreground: 'hsl(var(--card-foreground))'
+                },
+            },
+        },
+    },
+};
+
+
+// Debounce function to limit the rate at which `filterLinks` is called
+function debounce(func, wait) {
+    let timeout;
+    return function(...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(this, args), wait);
+    };
+}
+
+const debouncedFilterLinks = debounce(filterLinks, 300);
+
+document.getElementById('searchBox').addEventListener('input', debouncedFilterLinks);
+
+document.getElementById('gohome').addEventListener('click', function() {
+    window.location.href = 'index.html'; // Navigate to the home page
+});
+
+document.getElementById('darkModeToggle').addEventListener('click', function() {
+    document.body.classList.toggle('dark');
+});
