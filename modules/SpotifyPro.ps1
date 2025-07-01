9db4e55e96083e6fa29dc518649e349bc213702c
catsmoker <119059457+catsmoker@users.noreply.github.com>
2025-07-01 22:38:53 +0100

    Update SpotifyPro.ps1

--- modules/SpotifyPro.ps1
+++ modules/SpotifyPro.ps1
@@ -8,7 +8,14 @@ $progressBar.Value = 50
 $mainForm.Refresh()
 
 $statusLabel.Text = "Installing Spicetify for customization..."
-Start-Process "powershell" -ArgumentList "iwr -useb https://raw.githubusercontent.com/spicetify/cli/main/install.ps1 | iex" -Wait
+# Create temp BAT file
+$tempBat = "$env:TEMP\temp_spicetify_install.bat"
+Set-Content -Path $tempBat -Value '@echo off
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "iwr -useb https://raw.githubusercontent.com/spicetify/cli/main/install.ps1 | iex"
+del "%~f0"
+'
+# Run the BAT file
+Start-Process -FilePath $tempBat
 
 $progressBar.Value = 100
 $statusLabel.Text = "Installation complete!"
